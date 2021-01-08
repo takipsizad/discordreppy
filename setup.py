55f38a4e05e08fdbc0223053b5862915c557823c
takipsizad <addeniz25@gmail.com>
2021-01-08 12:14:51 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='discordrep',
+      version='0.1',
+      description='',
+      author='takipsizad',
+      packages=['discordrep'],
+      long_description='hey i dont know check url for this library',
+      url="https://github.com/takipsizad/discordreppy",
+      zip_safe=False)
